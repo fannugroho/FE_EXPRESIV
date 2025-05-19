141c49f389b5b4eed7e3331b39f3c8411200fdab
fannugroho <irfannugroho0407@gmail.com>
2025-05-19 15:23:09 +0700

    Add new pages and enhance navigation for Purchase Request and Settlement features

--- js/menuPR.js
+++ js/menuPR.js
@@ -94,21 +94,4 @@ startY: 20
 
 doc.save("Recent_Documents.pdf");
 }
-
-function goToMenu() { window.location.href = "../pages/menuPR.html"; }
-function goToAddDoc() {window.location.href = "../addPages/addPR.html"; }
-function goToAddReim() {window.location.href = "AddReim.html"; }
-function goToAddCash() {window.location.href = "AddCash.html"; }
-function goToAddSettle() {window.location.href = "AddSettle.html"; }
-function goToAddPO() {window.location.href = "AddPO.html"; }
-function goToMenuPR() { window.location.href = "MenuPR.html"; }
-function goToMenuReim() { window.location.href = "MenuReim.html"; }
-function goToMenuCash() { window.location.href = "MenuCash.html"; }
-function goToMenuSettle() { window.location.href = "MenuSettle.html"; }
-function goToApprovalReport() { window.location.href = "ApprovalReport.html"; }
-function goToMenuPO() { window.location.href = "MenuPO.html"; }
-function goToMenuInvoice() { window.location.href = "MenuInvoice.html"; }
-function goToMenuBanking() { window.location.href = "MenuBanking.html"; }
-function logout() { localStorage.removeItem("loggedInUser"); window.location.href = "Login.html"; }
-
-window.onload = loadDashboard;
+;
---
+++ pages/utils/toMenu.js
@@ -0,0 +1,22 @@
+function goToMenuPR() { window.location.href = "../pages/menuPR.html"; }
+function goToAddPR() {window.location.href = "../addPages/addPR.html"; }
+function goToAddReim() {window.location.href = "AddReim.html"; }
+function goToAddCash() {window.location.href = "AddCash.html"; }
+function goToAddSettle() {window.location.href = "AddSettle.html"; }
+function goToAddPO() {window.location.href = "AddPO.html"; }
+
+function goToDetailReim(reimId) {
+    window.location.href = `/detailPages/detailReim.html?reim-id=${reimId}`;
+}
+
+function goToMenuAPR() { window.location.href = "MenuPR.html"; }
+function goToMenuReim() { window.location.href = "MenuReim.html"; }
+function goToMenuCash() { window.location.href = "MenuCash.html"; }
+function goToMenuSettle() { window.location.href = "MenuSettle.html"; }
+function goToApprovalReport() { window.location.href = "ApprovalReport.html"; }
+function goToMenuPO() { window.location.href = "MenuPO.html"; }
+function goToMenuInvoice() { window.location.href = "MenuInvoice.html"; }
+function goToMenuBanking() { window.location.href = "MenuBanking.html"; }
+function logout() { localStorage.removeItem("loggedInUser"); window.location.href = "Login.html"; }
+
+window.onload = loadDashboard;
