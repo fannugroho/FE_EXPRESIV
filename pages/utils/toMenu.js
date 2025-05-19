9801fcf1d0ec80489c56b52af3f4214ca3536b04
fannugroho <irfannugroho0407@gmail.com>
2025-05-19 20:11:38 +0700

    Add new Cash Advance and Settlement pages, update menu styles and functionality
    - Introduced checkedCash.html and checkedCash.js for Cash Advance management.
    - Added checkedSettle.html and checkedSettle.js for Settlement management.
    - Updated menuSettle.css for improved button styling and hover effects.
    - Modified detailSettle.html to change the header from "Add Settlement" to "Settlement".
    - Created menuSettle.js to handle dashboard loading and navigation functionalities.
    - Updated toMenu.js for consistent navigation paths across the application.

--- pages/utils/toMenu.js
+++ pages/utils/toMenu.js
@@ -1,3 +1,4 @@
+function goToMenu() { window.location.href = "../pages/dashboard.html"; }
 function goToMenuPR() { window.location.href = "../pages/menuPR.html"; }
 function goToAddPR() {window.location.href = "../addPages/addPR.html"; }
 function goToAddReim() {window.location.href = "../addPages/AddReim.html"; }
@@ -10,11 +11,11 @@ function goToDetailReim(reimId) {
 }
 
 function goToMenuAPR() { window.location.href = "menuPR.html"; }
+function goToMenuPO() { window.location.href = "MenuPO.html"; }
 function goToMenuReim() { window.location.href = "menuReim.html"; }
 function goToMenuCash() { window.location.href = "menuCash.html"; }
 function goToMenuSettle() { window.location.href = "menuSettle.html"; }
 function goToApprovalReport() { window.location.href = "ApprovalReport.html"; }
-function goToMenuPO() { window.location.href = "MenuPO.html"; }
 function goToMenuInvoice() { window.location.href = "MenuInvoice.html"; }
 function goToMenuBanking() { window.location.href = "MenuBanking.html"; }
 function logout() { localStorage.removeItem("loggedInUser"); window.location.href = "Login.html"; }
