619444719d2ad73c6b437caec7e35478850e2408
rizkiagussalim <rizkiagussalim@apps.ipb.ac.id>
2025-05-26 17:04:58 +0700

    Update goToMenuSettle function to use relative path for navigation
    - Changed the URL in the goToMenuSettle function to point to the new relative path "../pages/MenuSettle.html" for improved navigation consistency.

--- js/addSettle.js
+++ js/addSettle.js
@@ -165,7 +165,7 @@ async function saveDocument() {
 }
 
 function goToMenuSettle() {
-    window.location.href = "MenuSettle.html";
+    window.location.href = "../pages/MenuSettle.html";
 }
 
 // Commented out because docType element doesn't exist in this HTML
